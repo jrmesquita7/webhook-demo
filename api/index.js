fb00129fc8092966c892764803ead6417d50cf6d
jrmesquita7 <cleobetojunior1999@gmail.com>
2024-11-03 12:55:24 -0300

    Configuração do webhook e estrutura de pastas

--- api/index.js
+++ api/index.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-// Middleware para interpretar JSON no corpo da requisição
 app.use(express.json());
 
-// Rota do webhook
 app.post('/webhook', (req, res) => {
-    const payload = req.body; // Aqui você recebe os dados enviados para o webhook
-    console.log('Webhook recebido:', payload); // Para teste, exiba no console
-
-    // Enviar uma resposta ao remetente
+    const payload = req.body;
+    console.log('Webhook recebido:', payload);
     res.status(200).json({ message: 'Webhook recebido com sucesso!' });
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
+module.exports = app;
