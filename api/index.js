33fb97296f46c2162761c1f7689f77a318a110fc
Junior Mesquita <79610977+jrmesquita7@users.noreply.github.com>
2024-11-04 14:21:21 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,14 +2,12 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Servidor funcionando!");
-});
-
+// Endpoint do webhook
 app.post("/webhook", (req, res) => {
     const payload = req.body;
     console.log("Webhook recebido:", payload);
     res.status(200).json({ message: "Webhook recebido com sucesso!" });
 });
 
+// Exporta o app como um manipulador de função para o Vercel
 module.exports = app;
