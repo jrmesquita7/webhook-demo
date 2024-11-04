aa7dfc12aa2d2da18cd833bc0347093949406bea
Junior Mesquita <79610977+jrmesquita7@users.noreply.github.com>
2024-11-04 11:09:39 -0300

    fix: corrijir configurações para o vercel

--- api/index.js
+++ api/index.js
@@ -1,12 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
 app.use(express.json());
 
-app.post('/webhook', (req, res) => {
+app.post("/webhook", (req, res) => {
     const payload = req.body;
-    console.log('Webhook recebido:', payload);
-    res.status(200).json({ message: 'Webhook recebido com sucesso!' });
+    console.log("Webhook recebido:", payload);
+    res.status(200).json({ message: "Webhook recebido com sucesso!" });
 });
 
 module.exports = app;
